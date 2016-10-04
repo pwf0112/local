220cb7ba995f046e2e0f54c8591b79e0b3eaabd7
Weifeng Peng <pwf0112@163.com>
2016-10-04 18:31:38 +0800

    modified base.blade

--- resources/views/inc/base.blade.php
+++ resources/views/inc/base.blade.php
@@ -2,16 +2,21 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Flat UI Free 101 Template</title>
+    <title>收银机</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('flat/css/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('flat/css/flat-ui.min.css') }}"  rel="stylesheet">
     <link rel="shortcut icon" href="{{ asset('flat/img/favicon.ico') }}">
+    <style>
+        ::-webkit-scrollbar {
+            width:0;
+        }
+    </style>
     @yield('head')
 </head>
 <body>
 @yield('header')
-<div class="container">
+<div class="container" style="margin-top: 80px; margin-bottom: 40px;">
     @yield('content')
 </div>
 @yield('bottom')
@@ -21,5 +26,10 @@
 <script src="{{ asset('flat/js/flat-ui.min.js') }}"></script>
 <script src="{{ asset('js/vue.min.js') }}"></script>
 @yield('boot')
+
+<footer style="height: 35px; line-height: 35px; text-align: center; position: fixed; bottom: 0; width: 100%;">
+    &copy; 忻州东大
+</footer>
+
 </body>
 </html>
