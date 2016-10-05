5b6c8f24ef88988ef575cffc2edfad4f41929a63
Weifeng Peng <pwf0112@163.com>
2016-10-05 18:59:02 +0800

    modified cash/add.blade vue组件部分实现——2 nedd fix

--- resources/views/vendor/cash/add.blade.php
+++ resources/views/vendor/cash/add.blade.php
@@ -14,12 +14,13 @@
 @stop
 
 @section('content')
+    <div id="form-cash-add">
     <div class="panel panel-default">
         <div class="panel-heading">
             <h3 class="panel-title"><span class="glyphicon glyphicon-plus"></span> 添加收银机</h3>
         </div>
         <div class="panel-body">
-            <form id="form-cash-add" class="form-horizontal" role="form" @submit.prevent="submit(form)">
+            <form class="form-horizontal" role="form" @submit.prevent="submit(form)">
                 <strong>基础信息</strong>
                 <hr>
                 <div class="form-group form-group-sm">
@@ -45,38 +46,41 @@
                 <div class="form-group form-group-sm">
                     <label class="col-sm-2 control-label" for="posi">主机</label>
                     <div class="col-sm-10">
-                        <button type="button" class="btn btn-danger btn-xs" @click="mps">添加主机</button>
+                        <span class="form-control-static form-control-static-extend"><span v-text="mac_txt"></span></span>
+                        <button type="button" class="btn btn-danger btn-xs" @click="choose('mac')">添加主机</button>
                     </div>
                 </div>
                 <div class="form-group form-group-sm">
                     <label class="col-sm-2 control-label">小票机</label>
                     <div class="col-sm-10">
-                        <button type="button" class="btn btn-danger btn-xs">添加小票机</button>
+                        <span class="form-control-static form-control-static-extend"><span v-text="pos_txt"></span></span>
+                        <button type="button" class="btn btn-danger btn-xs" @click="choose('pos')">添加小票机</button>
                     </div>
                 </div>
                 <div class="form-group form-group-sm">
                     <label class="col-sm-2 control-label" for="posi">POS机</label>
                     <div class="col-sm-10">
-                        <span class="form-control-static form-control-static-extend"><span v-text="form.pos.name"></span> - 203</span>
-                        <button type="button" class="btn btn-info btn-xs">变更POS机</button>
+                        <span class="form-control-static form-control-static-extend"><span v-text="bil_txt"></span></span>
+                        <button type="button" class="btn btn-info btn-xs" @click="choose('bil')">变更POS机</button>
                     </div>
                 </div>
                 <div class="form-group form-group-sm">
                     <label class="col-sm-2 control-label" for="posi">打印机</label>
                     <div class="col-sm-10">
-                        <button type="button" class="btn btn-danger btn-xs">添加打印机</button>
+                        <span class="form-control-static form-control-static-extend"><span v-text="pri_txt"></span></span>
+                        <button type="button" class="btn btn-danger btn-xs" @click="choose('pri')">添加打印机</button>
                     </div>
                 </div>
 
 
 
-                <strong>系统镜像</strong>
+                <strong>系统镜像 @{{ form.sys.id }}</strong>
                 <hr>
                 <div class="form-group form-group-sm">
                     <label class="col-sm-2 control-label" for="posi">选择镜像</label>
                     <div class="col-sm-5">
-                        <select class="form-control">
-                            <option>POS_WINXP_HK380_20160112</option>
+                        <select class="form-control" v-model="form.sys.id">
+                            <option value="12">POS_WINXP_HK380_20160112</option>
                             <option>POS_HK380_20160112</option>
                             <option>POS_HK380_20160112</option>
                             <option>POS_WINXP_HK380_20160112</option>
@@ -98,35 +102,31 @@
         </div>
     </div>
 
-    <div id="ooo" style="padding: 20px;">
+    <div id="ale" style="padding: 20px;">
         <form class="form-horizontal" role="form">
             <div class="form-group form-group-sm">
                 <label class="col-sm-2 control-label" for="posi">输入编号</label>
                 <div class="col-sm-5">
-                    <input class="form-control" type="text" id="posi" v-model="form.posi" placeholder="位置为3位数字编号">
+                    <input class="form-control" type="text" id="posi" v-model="com.code" placeholder="位置为3位数字编号">
                 </div>
             </div>
 
             <div class="form-group form-group-sm">
                 <label class="col-sm-2 control-label" for="posi">选择型号</label>
                 <div class="col-sm-8">
-                    <select class="form-control">
-                        <option>POS_WINXP_HK380_20160112</option>
-                        <option>POS_HK380_20160112</option>
-                        <option>POS_HK380_20160112</option>
-                        <option>POS_WINXP_HK380_20160112</option>
-                        <option>POS_HK380_20160112</option>
-                        <option>POS_HK380_20160112</option>
+                    <select class="form-control" v-model="index">
+                        <option v-for="(index, list) in lists" :value="index"> @{{ list.name }} </option>
                     </select>
                 </div>
             </div>
         </form>
-    </div>
+    {{--</div>--}}
+        {{--<div v-for="list in lists">@{{ list.name }}</div>--}}
+    {{--</div>--}}
 @stop
 
 @section('boot')
     <script>
-        $('#ooo').hide();
         new Vue({
             el: '#form-cash-add',
             data: {
@@ -152,24 +152,59 @@
                     bil: {
                         id: 2,
                         name: '通用小票打印机',
-                        code: ''
+                        code: '203'
                     },
                     sys: {
                         id: 1,
                         name:'WINXP_POS_HK380'
                     }
+                },
+                lists: [
+                    {id: 12, name: 'POS_12'},
+                    {id: 13, name: 'POS_13'},
+                    {id: 14, name: 'POS_14'},
+                    {id: 15, name: 'POS_15'}
+                ],
+                com: {
+                    id: 0,
+                    name: '',
+                    code: ''
+                },
+                index: null
+            },
+            computed: {
+                bil_txt: function () {
+                    return this.data_txt(this.form.bil);
+                },
+                pos_txt: function () {
+                    return this.data_txt(this.form.pos);
+                },
+                mac_txt: function () {
+                    return this.data_txt(this.form.mac);
+                },
+                pri_txt: function () {
+                    return this.data_txt(this.form.pri);
+                }
+            },
+            watch: {
+                'index': function (value) {
+                    this.com = this.lists[value];
                 }
             },
             methods: {
-                mps: function () {
+                choose: function (tag) {
+//                    layer.alert(this.form[tag].name);
                     layer.open({
+                        vue: this,
                         type: 1,
                         area: '510px',
                         title: '添加收银机',
-                        content: $('#ooo'),
+                        content: $('#ale'),
                         btn: ['确定', '取消'],
-                        yes: function (index, layero) {
-
+                        yes: function (index) {
+                            console.log([this.vue.com.code, tag]);
+                            this.vue.form[tag] = this.vue.com;
+                            layer.close(index);
                         },
                         cancel: function (index) {
 
@@ -180,8 +215,22 @@
                     //询问框
                     layer.alert('sdfsdfsdf');
 
+                },
+                data_txt: function (data) {
+                    if (data.name != '' && data.code != '') {
+                        return data.name + " - " + data.code;
+                    } else if (data.name != '') {
+                        return data.name;
+                    } else {
+                        return '';
+                    }
                 }
             }
         });
+
+        Vue.ready(function () {
+            $('#ale').hide();
+        });
+
     </script>
 @stop
