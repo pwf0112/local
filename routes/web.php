7779cb062ef5b93aa9a0e499d628e63a34512c24
PengWeifeng <pwf0112@163.com>
2016-10-07 22:17:56 +0800

    add getRoute cash/add/data
    此路由用于获取并初始化添加/编辑收银机的数据

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,41 @@ Route::get('cash/add', function () {
 	return view('vendor.cash.add');
 })->name('cash.add');
 
+Route::get('cash/add/data', function () {
+    $demo = [
+        ['id' => 1, 'name' => 'SYS_01'],
+        ['id' => 2, 'name' => 'SYS_02'],
+        ['id' => 3, 'name' => 'SYS_03'],
+        ['id' => 4, 'name' => 'SYS_04'],
+    ];
+
+    $lists = [
+        'mac' => $demo,
+        'pri' => $demo,
+        'pos' => $demo,
+        'bil' => $demo,
+        'sys' => $demo
+    ];
+
+    $form = [
+        'posi' => '',
+        'port' => '',
+        'ip' => '',
+        'sys' => null,
+        'mac' => ['id' => 1, 'code' => 'mac101'],
+        'pri' => ['id' => 4, 'code' => ''],
+        'bil' => ['id' => 2, 'code' => 'bil201'],
+        'pos' => ['id' => 3, 'code' => 'pos403'],
+    ];
+
+    $data = [
+        'form' => $form,
+        'lists' => $lists
+    ];
+
+    return $data;
+});
+
 Route::get('json', function () {
 	return [
 		['id' => 1, 'name' => 'POS_HK380_1'],
