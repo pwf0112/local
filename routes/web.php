94caab42ec3850cd9681a87b63f3f8f470df6a64
PengWeifeng <pwf0112@163.com>
2016-10-07 22:52:41 +0800

    add 收银机数据获取Controller,并关联至Route-cash/add/data.get

--- routes/web.php
+++ routes/web.php
@@ -19,49 +19,9 @@ Route::get('cash/add', function () {
 	return view('vendor.cash.add');
 })->name('cash.add');
 
-Route::get('cash/add/data', function () {
-    $demo = [
-        ['id' => 1, 'name' => 'SYS_01'],
-        ['id' => 2, 'name' => 'SYS_02'],
-        ['id' => 3, 'name' => 'SYS_03'],
-        ['id' => 4, 'name' => 'SYS_04'],
-    ];
+Route::get('cash/add/data', 'Cash\Data@handle');
 
-    $lists = [
-        'mac' => $demo,
-        'pri' => $demo,
-        'pos' => $demo,
-        'bil' => $demo,
-        'sys' => $demo
-    ];
-
-    $form = [
-        'posi' => '',
-        'port' => '',
-        'ip' => '',
-        'sys' => null,
-        'mac' => ['id' => 1, 'code' => 'mac101'],
-        'pri' => ['id' => 4, 'code' => ''],
-        'bil' => ['id' => 2, 'code' => 'bil201'],
-        'pos' => ['id' => 3, 'code' => 'pos403'],
-    ];
-
-    $data = [
-        'form' => $form,
-        'lists' => $lists
-    ];
-
-    return $data;
-});
-
-Route::get('json', function () {
-	return [
-		['id' => 1, 'name' => 'POS_HK380_1'],
-		['id' => 2, 'name' => 'POS_HK380_2'],
-		['id' => 3, 'name' => 'POS_HK380_3'],
-		['id' => 4, 'name' => 'POS_HK380_4']
-	];
-});
+Route::get('json', 'Cash\Data@handle');
 
 Route::post('post01', function (Request $request) {
 	\Log::alert($request->input());
